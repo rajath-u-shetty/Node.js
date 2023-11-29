f11dba9c3d7bfa84a3bcf0e32bad1da6f52df8a1
Rajath U Shetty <rajathushetty56@gmail.com>
2023-11-29 23:33:54 +0530

    Day 2

---
+++ Rest-Api/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs")
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/users", (req, res) => {
+    const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `
+    return res.send(html)
+})
+
+app.get("/api/users", (req, res) => {
+    return res.json(users);
+});
+
+app
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        const body = req.body;
+
+        return res.json({status: "pending"})
+    })
+    .delete((req, res) => {
+        const id = Number(req.params.id);
+        const popId = users.findIndex((user) => user.id === id)
+        console.log(popId);
+        if (popId !== -1) {
+            users.splice(popId, 1);
+                fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, req) => {
+                    return console.log("success");
+                })
+        }
+    });
+
+
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
+        return res.json({status: "success"})
+    })    
+})
+
+
+app.listen(port, ()=> console.log("server started"));
